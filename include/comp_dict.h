44c8e613394c762712732847c9209dbec3fe8335
Otávio Carvalho <omcarvalho@inf.ufrgs.br>
2014-08-20 22:21:51 -0300

    hash table functions

--- include/comp_dict.h
+++ include/comp_dict.h
@@ -1,18 +1,34 @@
-// Estruturas do dicionário
+#include <stdlib.h>
+#include <stdio.h>
+#include <search.h>
+#include <string.h>
+
+#define SIZE_TABLE_KEY 110
+
 typedef struct comp_dict_item_t comp_dict_item_t;
 struct comp_dict_item_t {
-    char *key;
-    char *value;
+    int line;
 };
 
 typedef struct comp_dict_t comp_dict_t;
 struct comp_dict_t {
+    char key[SIZE_TABLE_KEY];
     comp_dict_item_t item;
     comp_dict_t *next;
 };
 
-// Variável global do escopo corrente
-comp_dict_t* symbol_table_cur_scope;
+typedef struct comp_dict_tree_t {
+    int id;
+    struct comp_dict_tree_t *next_brother;
+    struct comp_dict_tree_t *sons;
+} comp_dict_tree_t ;
 
 // Diretivas externas
 extern char* yytext;
+
+// Funções
+comp_dict_tree_t* init_table_tree();
+void free_table_tree(comp_dict_tree_t* tree);
+comp_dict_t* add_symbol(comp_dict_tree_t* cur_table, char* key, int line);
+comp_dict_t* find_symbol(comp_dict_tree_t* cur_table, char* key);
+void print_symbol(comp_dict_tree_t* cur_table);
--- src/comp_dict.c
+++ src/comp_dict.c
@@ -2,7 +2,58 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <search.h>
+#include <string.h>
 
-comp_dict_item_t* add_symbol(comp_dict_item_t* dict, char* yytext, int ident){
-    /*return item;*/
+// Inicia a árvore de tabelas
+comp_dict_tree_t* init_table_tree(){
+    comp_dict_tree_t* root = malloc(sizeof(comp_dict_tree_t));
+    root->id = 1;
+    root->sons = NULL;
+    root->next_brother = NULL;
+
+    return root;
+}
+
+// Libera o conteúdo alocado pelo ponteiro da raiz da árvore
+void free_table_tree(comp_dict_tree_t* tree){
+    free(tree);
+}
+
+// Adiciona um símbolo a uma tabela existente
+comp_dict_t* add_symbol(comp_dict_tree_t* cur_table, char* key, int line){
+    ENTRY e, *e_search;
+    char *table_key = malloc(sizeof(char)*SIZE_TABLE_KEY);
+
+    // Prepara entrada de dicionário a ser adicionada na tabela hash
+    comp_dict_t* data = malloc(sizeof(comp_dict_t));
+    strcpy(data->key, yytext);
+    data->item.line = line;
+    data->next = NULL;
+
+    // Insere na tabela
+    e.key = strcat(table_key, key);
+    e.data = data;
+    e_search = hsearch(e, ENTER);
+    if (e_search == NULL) {
+        fprintf(stderr, "Error: insert on table failed\n");
+        exit(1);
+    }
+
+    return e_search->data;
+}
+
+// Procura um símbolo em uma tabela existente
+comp_dict_t* find_symbol(comp_dict_tree_t* cur_table, char* key){
+    ENTRY e, *ep;
+    comp_dict_t* data = malloc(sizeof(comp_dict_t));
+
+    e.key = key;
+    ep = hsearch(e, FIND);
+
+    if (ep != NULL){
+        memcpy(data, ep->data, sizeof(comp_dict_t *));
+        return ep->data;
+    }
+
+    return NULL;
 }
