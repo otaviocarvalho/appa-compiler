a320e1bed2342c09266f4464f430819e22a76e05
Otávio Carvalho <omcarvalho@inf.ufrgs.br>
2014-08-20 22:23:06 -0300

    global variables / init and finalize directives

--- include/misc.h
+++ include/misc.h
@@ -2,11 +2,14 @@
 #define __MISC_H
 #include <stdio.h>
 #include <search.h>
+#include "comp_dict.h"
 
 #define HASH_SIZE 1000000
 
 int cur_line = 1;   // Inicializa o compilador na linha 1
-struct comp_dict_t* symbol_table_cur_scope; // Ponteiro para a tabela de símbolos do escopo corrente
+
+struct comp_dict_tree_t* symbol_table_root; // Ponteiro para a raiz da tabela de símbolos
+struct comp_dict_tree_t* symbol_table_cur; // Ponteiro para a tabela de símbolos do escopo corrente
 
 int getLineNumber (void);
 void yyerror (char const *mensagem);
--- src/misc.c
+++ src/misc.c
@@ -1,21 +1,20 @@
 #include "misc.h"
 
-int getLineNumber (void)
-{
+int getLineNumber (void){
     return cur_line;
 }
 
-void yyerror (char const *mensagem)
-{
+void yyerror (char const *mensagem){
     fprintf (stderr, "%s (line %d)\n", mensagem, cur_line);
 }
 
-void main_init (int argc, char **argv)
-{
+void main_init (int argc, char **argv){
     hcreate(HASH_SIZE);
+    symbol_table_root = init_table_tree();
+    symbol_table_cur = symbol_table_root;
 }
 
-void main_finalize (void)
-{
-
+void main_finalize (void){
+    hdestroy();
+    free_table_tree(symbol_table_root);
 }
