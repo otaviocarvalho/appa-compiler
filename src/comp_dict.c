2fc084ed21ff8ca96173b1ad4da719fa83483399
Otávio Carvalho <omcarvalho@inf.ufrgs.br>
2014-09-15 21:44:23 -0300

    Alteração do union para identificadores

--- src/comp_dict.c
+++ src/comp_dict.c
@@ -107,17 +107,17 @@ comp_dict_item_t* add_symbol(comp_dict_t* cur_table, char* key, int line, int ty
 
     node->key = malloc(sizeof(char)*SIZE_TABLE_KEY);
     strcpy(node->key, key);
-    
+
     node->next = NULL;
     node->item->line = line;
     node->item->type = convert_type_symbol(type);
     node->item->value = alloc_value_symbol(node->item->type, key); // Aloca valor do token de acordo com o tipo
-   
+
     char buffer[SIZE_TABLE_KEY];
     snprintf(buffer,SIZE_TABLE_KEY,"%d",node->item->type);
     strcat(node->key, buffer);
- 
-    
+
+
     // Calcula o hash
     int hash = hash_function(node->key);
     // Inicializa bucket com esse elemento se não encontrar nenhum
